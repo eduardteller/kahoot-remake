7b337386ea02b3a00cd7a8d86b6d92a706e62fd6
Eduard <edtell@taltech.ee>
2024-08-10 17:49:59 +0300

    12312312

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
-import "./index.css";
+// import KahootIcon from "./assets/kahoot.svg";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <>
-      <main className="h-screen w-screen bg-black">
+      <header className="flex h-24 w-screen items-center justify-center">
+        <div className="flex h-full w-1/2 items-center justify-center">
+          <img className="h-full" src="src/assets/kahoot.svg" alt="" />
+        </div>
+        {/* <KahootIcon /> */}
+      </header>
+      <main className="h-screen w-screen bg-base-200">
         <div className="flex h-full w-full items-center justify-center">
           <button
             onClick={() => setCount(count + 1)}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./input.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 );
